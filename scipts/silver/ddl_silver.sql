c2e0a23cdb7522b22db366379dc4595e3f4379d6
Tapan Dey <tapandey27@gmail.com>
2025-12-28 16:19:07 +0530

    Update ddl_silver.sql

--- scipts/silver/ddl_silver.sql
+++ scipts/silver/ddl_silver.sql
@@ -1,1 +1,107 @@
+/*
+========================================================
+DDL Scripts: Create Silver Tables
+========================================================
+Script Purpose:
+  This script creates tables in the 'silver' schema, dropping any existing tables if they already exist. 
+  Run this script to re-defind the DDL Structure of 'silver' Tables.
+  /*
+
+/*
+Silver Rules:
+All names must start with the source system name, and table names must match their original names without renaming.
+<sourcesystem>_<entity>
+<sourcesystem>: Name of the source system (e.g. crm, erp)
+<entity> : Exact table name from the source system.
+Example: crm_customer_info ? Customer information from the CRM system.
+*/
+
+USE DataWarehouseDB;
+GO
+--CRM Tables
+IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_cust_info;
+-- schema.<sourcesystem>_<entity>
+CREATE TABLE silver.crm_cust_info 
+(
+	cst_id				INT,
+	cst_key				NVARCHAR(50),
+	cst_firstname		NVARCHAR(50),
+	cst_lastname		NVARCHAR(50),
+	cst_marital_status	NVARCHAR(50),
+	cst_gndr			NVARCHAR(50),
+	cst_create_date		DATE,
+	dwh_create_date		DATETIME2 DEFAULT GETDATE() -- This is added to record when the data was last added.
+);
+GO
+
+IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_prd_info;
+CREATE TABLE silver.crm_prd_info 
+(
+	prd_id			INT,
+	cat_id			NVARCHAR(50),
+	prd_key			NVARCHAR(50),
+	sales_prd_key	NVARCHAR(50),
+	prd_nm			NVARCHAR(50),
+	prd_cost		INT,
+	prd_line		NVARCHAR(50),
+	prd_start_dt	DATE,
+	prd_end_dt		DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE() -- This is added to record when the data was last added.
+);
+GO
+
+IF OBJECT_ID ('silver.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details
+(
+	sls_ord_num		NVARCHAR(50),
+	sls_prd_key		NVARCHAR(50),
+	sls_cust_id		INT,
+	sls_order_dt	DATE,
+	sls_ship_dt		DATE,
+	sls_due_dt		DATE,
+	sls_sales		INT,
+	sls_quantity	INT,
+	sls_price		INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE() -- This is added to record when the data was last added.
+);
+GO
+
+-- ERP Tables
+
+IF OBJECT_ID ('silver.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE silver.erp_cust_az12;
+CREATE TABLE silver.erp_cust_az12
+(
+	CID				NVARCHAR(50),
+	BDATE			DATE,
+	GEN				NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE() -- This is added to record when the data was last added.
+);
+GO
+ 
+IF OBJECT_ID ('silver.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE silver.erp_loc_a101;
+CREATE TABLE silver.erp_loc_a101
+(
+	CID				NVARCHAR(50),
+	CNTRY			NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE() -- This is added to record when the data was last added.
+);
+GO
+
+IF OBJECT_ID ('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE silver.erp_px_cat_g1v2;
+CREATE TABLE silver.erp_px_cat_g1v2
+(
+	ID				NVARCHAR(50),
+	CAT				NVARCHAR(50),
+	SUBCAT			NVARCHAR(50),
+	MAINTENANCE		NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE() -- This is added to record when the data was last added.
+);
+GO
+
 
